e740187b46ee68315455032235ecd7b5961c8e51
schop23 <schop23@gmail.com>
2022-09-25 15:48:59 -0400

    flask route update

--- imFlask.py
+++ imFlask.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
+@app.route('templates/about')
 def about():
     return render_template('about.html')
 
