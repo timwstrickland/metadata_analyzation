175d56e1723717ab8275c0d290140c9dbe8cc3cd
schop23 <schop23@gmail.com>
2022-09-25 15:57:27 -0400

    attempt to fix about pg routing 2nd attempt

--- imFlask.py
+++ imFlask.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/templates/about')
+@app.route('/about')
 def about():
     return render_template('about.html')
 
